7432e9ab6ddf9eebeb8da57411484765c18fbcfc
nomadWebDeveloper <kgjsmart1559@gmail.com>
2022-09-05 16:39:16 +0900

    login-session

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'ejs'); //npm install ejs
 app.use('/public', express.static('public')); // 미들웨어, static 파일을 보관하기 위해 public 폴더를 사용할 것임을 명시. 서버 중간에 작동
 const MongoClient = require('mongodb').MongoClient;
+const passport = require('passport'); // install
+const LocalStrategy = require('passport-local').Strategy;//install
+const session = require('express-session');//install
+app.use(session({secret : 'secretCode', resave : true, saveUninitialized : false}));
+app.use(passport.initialize());
+app.use(passport.session());
+//app.use(미들웨어) : 요청 - 응답 중간에 뭔가 실행되는 코드
 
 
 
@@ -120,5 +127,62 @@ app.put('/edit', function(req, res) { // _id를 url의 파라미터에 넣어서
         console.log('수정완료');
         res.redirect('/list');
     });
-})
-           
+});
+
+app.get('/login', function(req, res){
+    res.render('login.ejs');
+});
+
+app.post('/login', passport.authenticate('local', {
+    failureRedirect : '/fail'
+}) /* 검사 */, function(req, res){
+    res.redirect('/');
+});
+
+app.get('/mypage', loginCheck() /* 미들웨어 */, function(req, res) {
+    console.log(req.user);
+    res.render('mypage.ejs', {user : req.user});
+});
+
+//미들웨어 함수 생성
+function loginCheck(req, res, next) { // 로그인 후 세션이 있으면 req.user 가 항상 있음
+    if (req.user) {
+        next()
+    } else {
+        res.send('로그인 후 이용해주세요');
+    }
+}
+
+// 로그인 인증하는 코드
+passport.use(new LocalStrategy({
+    usernameField : 'id',
+    passwordField : 'pw',
+    session : true, // 로그인 후 세션을 저장할 것인지
+    passReqToCallback : false, //아이디 비밀번호 말고 다른 정보 검증 시
+}, function(inputId, inputPw, done) {
+    console.log(inputId, inputPw);
+    db.collection('login').findOne({id : inputId}, function(error, result){
+        if (error) return done(error)
+        if (!result) return done(null, false, {message : '존재하지 않는 아이디입니다.'}) // result 가 아무것도 없을 때, null은 에러처리
+        if (inputPw == result.pw) {
+            return done(null, result)
+        } else {
+            return done(null, false, {message : '비밀번호가 일치하지 않습니다.'})
+        }
+    });
+})); 
+
+
+// 세션 + 쿠키 만들어 줌
+passport.serializeUser(function(user, done){  // id를 이용해서 세션을 저장시키는 코드 (로그인 성공시 발동), user 은 위 코드에서의 result 값임
+    done(null, user.id);
+});
+
+//위의 user.id 와 밑의 id 는 동일함
+passport.deserializeUser(function(id, done) { //마이페이지 접속시 발동, 로그인한 유저의 세션아이디를 바탕으로 개인정보를 db에서 찾는 역할
+    db.collection('login').fineOne({id : id}, function(error, result) {
+        if (error) return console.log(error);
+        done(null, result); 
+    })
+    
+});
